3ecd7308827378bbfc49219932e502475b052fe5
Youssef Zayed <youssef.Ah.zayed@gmail.com>
2024-11-29 16:11:11 -0500

    add authentication screens and layout components

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
-
+import { createAccount } from '../lib/appwrite'
 const App = () => {
   return (
     <View className='flex-1 items-center justify-center bg-white'>
-      <Text className='text-3xl font-bold text-center text-black'>
-        Virtual Trainer 
-     </Text>
-     <StatusBar style="auto" />
+      <Text className='text-3xl text-center font-pblack'>
+        Virtual Trainer
+      </Text>
+      <TouchableOpacity className='bg-blue-500 p-2 rounded-md' onPress={() => createAccount()}>
+        <Text className='text-white'>Sign In</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
     </View>
   )
 }
